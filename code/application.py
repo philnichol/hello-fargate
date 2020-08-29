b8c1a4b2368a8f0fa29fc54d77219fffcff32bd0
philnichol <philipebnichol@gmail.com>
2020-08-29 20:13:43 +0100

    added code deployment;

---
+++ code/application.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import datetime, os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    greeting = os.environ.get("GREETING", "hello CTM!")
+    time_format = os.environ.get("TIME_FORMAT", "%d/%m/%Y, %H:%M:%S")
+    now = datetime.datetime.now().strftime(time_format)
+    return f"{greeting}, it is {now}\n"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=5000)
