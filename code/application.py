c9b86cd6027e70c96c9715344c86df5d641add77
philnichol <philipebnichol@gmail.com>
2020-08-30 12:12:22 +0100

    actioned test results

--- code/application.py
+++ code/application.py
@@ -1,17 +1,19 @@
 from flask import Flask
-import datetime, os
+from datetime import datetime
+from os import environ
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     # get greeting/timeformat from envvars, with sensible defaults
-    greeting = os.environ.get("GREETING", "hello CTM!")
-    time_format = os.environ.get("TIME_FORMAT", "%d/%m/%Y, %H:%M:%S")
-    now = datetime.datetime.now().strftime(time_format)
+    greeting = environ.get("GREETING", "hello CTM!")
+    time_format = environ.get("TIME_FORMAT", "%d/%m/%Y, %H:%M:%S")
+    now = datetime.now().strftime(time_format)
     return f"{greeting}, it is {now}\n"
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("port", 5000))
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    port = int(environ.get("port", 5000))
+    app.run(debug=False, host="127.0.0.1", port=5000)
